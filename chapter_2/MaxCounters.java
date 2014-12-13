6af5b07507271cfc06d341d38d5a6b2f708f007c
asbereth <asbereth.susanto@gmail.com>
2014-12-13 17:02:15 -0500

    Create MaxCounters.java
    MaxCounters problem on Codility. This code achieves a score of 100 percent on both correctness and performance (O(M+N)). 
    
    The key idea here is to make sure that we check only for the maximum value up to the last time N+1 occurs in A, then from that point on, start updating the counter accordingly.

---
+++ chapter_2/MaxCounters.java
@@ -0,0 +1,53 @@
+
+
+// you can also use imports, for example:
+import java.util.Arrays;
+
+// you can use System.out.println for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(int N, int[] A) {
+        int M = A.length, currMax = 0, lastMaxCounter = 0;
+        int lastIndexOfMaxCounter = -1;
+        int counterSum = 0;
+        int counter[] = new int[N];
+        
+        for (int k = M-1; k >= 0; --k) {
+            if(A[k] == N+1) {
+                lastIndexOfMaxCounter = k;
+                break;
+            }
+        }
+        
+        if (lastIndexOfMaxCounter == -1) {
+            for (int l = 0; l < M; ++l) {
+                counter[A[l]-1] = counter[A[l]-1] + 1;
+            }
+            return counter;
+        }
+        
+        for (int n = 0; n <= lastIndexOfMaxCounter; ++n) {
+            if (A[n] == N+1) {
+                lastMaxCounter = currMax;
+            } else {
+                counter[A[n]-1] = counter[A[n]-1] > lastMaxCounter ? 
+                    counter[A[n]-1]+1 : lastMaxCounter + 1;
+                currMax = 
+                    counter[A[n]-1] > currMax ? 
+                    counter[A[n]-1] : currMax;
+            }
+        }
+        
+        for (int o = 0; o < N; ++o) {
+            counter[o] = currMax;
+        }
+        
+        for (int p = lastIndexOfMaxCounter + 1; p < M; ++p) {
+            counter[A[p]-1] = counter[A[p]-1] + 1;
+        }
+        
+        return counter;
+    }
+}
+
